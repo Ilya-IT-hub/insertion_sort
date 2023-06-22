21cebc1e2393faac4273511d2d4ff7385acd769c
Ilya-IT-hub <134401003+Ilya-IT-hub@users.noreply.github.com>
2023-06-22 13:09:07 +0300

    Create kt_3.py

---
+++ kt_3.py
@@ -0,0 +1,8 @@
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        value = array[i]
+        position = i
+        while position > 0 and array[position - 1] > value:
+            array[position] = array[position - 1]
+            position -= 1
+        array[position] = value
